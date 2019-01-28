2ac419d1b15aedb8d13bddaa0d22d196e8337aec
Wuzhang Fang <wfang@huskers.unl.edu>
2019-01-28 14:38:16 -0600

    remove NNTP part, change the regex code

--- project4_in_the_news/second_implementation/newsagent2.py
+++ project4_in_the_news/second_implementation/newsagent2.py
@@ -5,21 +5,24 @@ import re
 
 class NewsAgent:
     """
-    An object that can distribute news items from news sources to news destinations.
+    An object that can distribute news items from news sources to news
+    destinations.
     """
+
     def __init__(self):
         self.sources = []
         self.destinations = []
 
     def add_source(self, source):
         self.sources.append(source)
-    
+
     def addDestination(self, dest):
         self.destinations.append(dest)
-    
+
     def distribute(self):
         """
-        Retrieve all news items from all sources, and Distributes them to all destinations.
+        Retrieve all news items from all sources, and Distribute them to all
+        destinations.
         """
         items = []
         for source in self.sources:
@@ -35,37 +38,17 @@ class NewsItem:
         self.title = title
         self.body = body
 
-class NNTPSource:
-    """
-    A news source that retrieves news items from an NNTP group.
-    """
-    def __init__(self, servername, group, howmany):
-        self.servername = servername
-        self.group = group
-        self.howmany = howmany
-    
-    def get_items(self):
-        server = NNTP(self.servername)
-        resp, count, first, last, name = server.group(self.group)
-        start = last - self.howmany + 1
-        resp, overviews = server.over((start, last))
-        for id, over in overviews:
-            title = decode_header(over['subject'])
-            resp, info = server.body(id)
-            body = '\n'.join(line.decode('latin') for line in info.lines) + '\n\n'
-            yield NewsItem(title, body)
-        server.quit()
-
 class SimpleWebSource:
     """
-    A news source that extracts news items from a web page using regular expressions.
+    A news source that extracts news items from a web page using regular
+    expressions.
     """
     def __init__(self, url, title_pattern, body_pattern, encoding='utf8'):
         self.url = url
         self.title_pattern = re.compile(title_pattern)
         self.body_pattern = re.compile(body_pattern)
         self.encoding = encoding
-    
+
     def get_items(self):
         text = urlopen(self.url).read().decode(self.encoding)
         titles = self.title_pattern.findall(text)
@@ -91,6 +74,7 @@ class HTMLDestination:
         self.filename = filename
 
     def receive_items(self, items):
+
         out = open(self.filename, 'w')
         print("""
         <html>
@@ -100,44 +84,46 @@ class HTMLDestination:
           <body>
           <h1>Today's News</h1>
         """, file=out)
+
         print('<ul>', file=out)
         id = 0
         for item in items:
             id += 1
-            print('   <li><a href="#{}">{}</a></li>'.format(id, item.title), file=out)
+            print('  <li><a href="#{}">{}</a></li>'
+                    .format(id, item.title), file=out)
         print('</ul>', file=out)
+
         id = 0
         for item in items:
-            print('<h2><a name="{}">{}</a></h2>'.format(id, item.title), file=out)
+            id += 1
+            print('<h2><a name="{}">{}</a></h2>'
+                    .format(id, item.title), file=out)
             print('<pre>{}</pre>'.format(item.body), file=out)
+
         print("""
           </body>
         </html>
         """, file=out)
 
 def runDefaultSetup():
-     """
-     A default setup of sources and destination. Modify to taste.
-     """
-     agent = NewsAgent()
+    """
+    A default setup of sources and destination. Modify to taste.
+    """
+    agent = NewsAgent()
 
-     # A SimpleWebSource that retrieves news from Reuters:
-     reuters_url = 'https://www.reuters.com/news/world'
-     reuters_title = r'<h2><a href="[^"]*"\s*>(.*?)</a>'
-     reuters_body = r'</h2><p>(.*?)</p>'
-     reuters = SimpleWebSource(reuters_url, reuters_title, reuters_body)
-     agent.add_source(reuters)
-     # An NNTPSource that retrieves news from comp.lang.python.announce:
-     #clpa_server = 'news.foo.bar'
-     #clpa_server = 'news.ntnu.no'
-     #clpa_group = 'comp.lang.python.announce'
-     #clpa_howmany = 10
-     #clpa = NNTPSource(clpa_server, clpa_group, clpa_howmany)
-     #agent.add_source(clpa)
-     # Add destination
-     agent.addDestination(PlainDestination())
-     agent.addDestination(HTMLDestination('news.html'))
-     # Distribute the news items:
-     agent.distribute()
+    # A SimpleWebSource that retrieves news from Reuters:
+    reuters_url = 'http://www.reuters.com/news/world'
+    reuters_title = r'<title.*?>(.+?)</title>'
+    reuters_body = ''
+    reuters = SimpleWebSource(reuters_url, reuters_title, reuters_body)
+
+    agent.add_source(reuters)
+
+    # Add plain-text destination and an HTML destination:
+    agent.addDestination(PlainDestination())
+    agent.addDestination(HTMLDestination('news.html'))
+
+    # Distribute the news items:
+    agent.distribute()
 
 if __name__ == '__main__': runDefaultSetup()
