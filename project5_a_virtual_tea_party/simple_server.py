39b13a1944488f3d2a3d439f39c38e944c0fb9ff
WuzhangFang <wfang@huskers.unl.edu>
2019-02-20 21:48:03 -0600

    Update simple_server.py

--- project5_a_virtual_tea_party/simple_server.py
+++ project5_a_virtual_tea_party/simple_server.py
@@ -1,16 +1,22 @@
 from asyncore import dispatcher
 import asyncore, socket
 
+PORT = 5005
+
 class ChatServer(dispatcher):
+    def __init__(self,port):
+        dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(('',port))
+        self.listen(5)
     def handle_accept(self):
         conn, addr = self.accept()
         print('Connection attempt from', addr[0])
-
-s = ChatServer()
-s.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 5005))
-s.listen(5)
-asyncore.loop()
+if __name__ == '__main__':
+    s = ChatServer(PORT)
+    try: asyncore.loop()
+    except KeyboardInterrupt: pass
 
 # $ python3 simple_server.py
 # $ telnet localhost 5005
